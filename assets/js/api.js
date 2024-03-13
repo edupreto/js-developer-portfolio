7dfd248421c024aae1d6772f0d8548e64790ddea
Eduardo Avelino <avelino.edu@gmail.com>
2024-03-13 12:20:06 -0300

    api correction

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,7 +1,7 @@
 
 
 async function fetchProfileData () {
-    const url = '';
+    const url = 'https://raw.githubusercontent.com/edupreto/js-developer-portfolio/main/data/profile.json';
     const fetching = await fetch(url)
     return await fetching.json()
 }
