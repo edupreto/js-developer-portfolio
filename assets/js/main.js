326beca57f71540a7f557741f292504613330c6e
Eduardo Avelino <avelino.edu@gmail.com>
2024-03-18 11:38:29 -0300

    corrigindo  hardSkills

--- assets/js/main.js
+++ assets/js/main.js
@@ -16,12 +16,13 @@ function updateProfileInfo(profileData) {
 
     const phone = document.getElementById('profile.phone')
     phone.innerText = profileData.phone
-    phone.href = `tel:${profile.phone}`
+    phone.href = `tel:${profileData.phone}`
 
     const email = document.getElementById('profile.email')
     email.innerText = profileData.email
-    email.href = `mailto:${profile.email}`
+    email.href = `mailto:${profileData.email}`
 
+    
 }
 
 function updateSoftSkills(profileData) {
@@ -29,8 +30,17 @@ function updateSoftSkills(profileData) {
     softSkills.innerHTML = profileData.skills.softSkills.map(skill => `<li>${skill}</li>`).join('')
 }
 
+function updateHardSkills(profileData) {
+    const hardSkills = document.getElementById('profile.skills.hardSkills')
+    hardSkills.innerHTML = profileData.skills.hardSkills.map(skill => `<li><img src=${skill.logo} alt=${skill.name} title=${skill.name}></li>`).join('')
+}
+
+
 (async () => {
     const profileData = await fetchProfileData()
     updateProfileInfo(profileData)
     updateSoftSkills(profileData)
+    updateHardSkills(profileData)
+    
 })()
+
